cbbd112f92dcc8441145ccdbc4e82e7acaffc0bf
angginaruto <angginaruto774@gmail.com>
2026-05-22 08:18:47 +0700

    add

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,39 +1,3 @@
-// // src/services/auth.service.ts
-// import api from "@/lib/axios";
-// import type { AuthUser } from "@/store/auth.store";
-
-// export interface RegisterPayload {
-//   name: string;
-//   email: string;
-//   password: string;
-//   role: "CUSTOMER" | "ORGANIZER";
-//   referralCode?: string;
-// }
-
-// export interface LoginPayload {
-//   email: string;
-//   password: string;
-// }
-
-// export async function registerApi(payload: RegisterPayload): Promise<AuthUser> {
-//   const res = await api.post("/auth/register", payload);
-//   return res.data.data;
-// }
-
-// export async function loginApi(payload: LoginPayload): Promise<AuthUser> {
-//   const res = await api.post("/auth/login", payload);
-//   return res.data.data;
-// }
-
-// export async function logoutApi(): Promise<void> {
-//   await api.post("/auth/logout");
-// }
-
-// export async function getMeApi(): Promise<AuthUser> {
-//   const res = await api.get("/auth/me");
-//   return res.data.data;
-// }
-
 import api from "../lib/axios";
 import type { AuthUser } from "@/store/auth.store";
 
